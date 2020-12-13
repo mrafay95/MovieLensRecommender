13920e70044968bc8905145de709628899af76be
muhammad.rafay <muhammad.rafay@rolustech.net>
2020-12-13 11:08:20 +0500

    System1

--- System1.R
+++ System1.R
@@ -2,6 +2,9 @@
 
 library(recommenderlab)
 library(Matrix)
+library(data.table)
+library(Xmisc)
+
 
 
 myurl = "https://liangfgithub.github.io/MovieData/"
@@ -12,6 +15,11 @@ ratings = read.csv(paste0(myurl, 'ratings.dat?raw=true'),
                    header = FALSE)
 colnames(ratings) = c('UserID', 'MovieID', 'Rating', 'Timestamp')
 ratings$Timestamp = NULL
+
+
+
+
+
 
 myurl = "https://liangfgithub.github.io/MovieData/"
 movies = readLines(paste0(myurl, 'movies.dat?raw=true'))
@@ -28,22 +36,37 @@ movies$image_url = sapply(movies$MovieID,
 
 
 
+genres = as.data.frame(movies$Genres, stringsAsFactors=FALSE)
+tmp = as.data.frame(tstrsplit(genres[,1], '[|]', type.convert=TRUE), stringsAsFactors=FALSE)
+
+
+genre_list = c("Action", "Adventure", "Animation", 
+               "Children's", "Comedy", "Crime",
+               "Documentary", "Drama", "Fantasy",
+               "Film-Noir", "Horror", "Musical", 
+               "Mystery", "Romance", "Sci-Fi", 
+               "Thriller", "War", "Western")
+
+
+m = length(genre_list)
+genre_matrix = matrix(0, nrow(movies), length(genre_list))
+for(i in 1:nrow(tmp)){
+  genre_matrix[i,genre_list %in% tmp[i,]]=1
+}
+colnames(genre_matrix) = genre_list
+rownames(genre_matrix) = movies$MovieID
+
+remove("tmp", "genres")
 
 
 
-
-set.seed(100)
-train.id = sample(nrow(ratings), floor(nrow(ratings)) * 0.8)
-train = ratings[train.id, ]
-head(train)
-
-test = ratings[-train.id, ]
-head(test)
+genre_matrix = genre_matrix[unique(train$MovieID),]
 
 
-i = paste0('u', train$UserID)
-j = paste0('m', train$MovieID)
-x = train$Rating
+
+i = paste0('u', ratings$UserID)
+j = paste0('m', ratings$MovieID)
+x = ratings$Rating
 tmp = data.frame(i, j, x, stringsAsFactors = T)
 Rmat = sparseMatrix(as.integer(tmp$i), as.integer(tmp$j), x = tmp$x)
 rownames(Rmat) = levels(tmp$i)
@@ -51,17 +74,48 @@ colnames(Rmat) = levels(tmp$j)
 Rmat = new('realRatingMatrix', data = Rmat)
 
 
-rec_UBCF = Recommender(Rmat, method = 'UBCF',
-                       parameter = list(normalize = 'Z-score', 
-                                        method = 'Cosine', 
-                                        nn = 25))
 
 
-recom = predict(rec_UBCF, Rmat[100:105], type = 'ratings')
+movieAvgRat  = colMeans(as(Rmat, 'matrix'), na.rm = TRUE) 
 
 
-as(recom, 'matrix')[, 1:10]
-as(Rmat, 'matrix')[100:105, 1:10]
+genre_highly_rated_matrix = matrix(NA, 18, length(unique(ratings$MovieID)))
+rownames(genre_highly_rated_matrix) = genre_list
+
+
+Rmat_Cols  = strtoi(strip(colnames(Rmat), char="m"))
+
+for(i in 1:18) {
+  
+  tmp = data.frame(id = movies$MovieID[genre_matrix[,i] == 1], rat = 1:length(movies$MovieID[genre_matrix[,i] == 1]))
+  tmp[,2] =NA
+  
+  tmp2 = movieAvgRat[Rmat_Cols %in% movies$MovieID[genre_matrix[,i] == 1]] 
+  
+  for(j in tmp$id){
+    if(!is.na(j)){
+
+      tmp[which(tmp$id == j),2] = tmp2[paste0('m',j)]
+    
+    }
+  }
+  
+  tmp = tmp[order(tmp$rat,decreasing = TRUE),]
+  print(i)
+  for(k in 1:dim(tmp)[1]){
+    if(!is.na(tmp[k,2])){
+      genre_highly_rated_matrix[i,k] = tmp[k,1]
+    }
+    
+  }
+
+}
+
+
+
+
+
+
 
 
 
